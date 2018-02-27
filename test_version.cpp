adb58a6fbf4546142430616fa9c9a708429ca75d
Vitaly Razgulyaev <razgulyaev.v@alager.ru>
2018-02-27 17:40:13 +0300

    Fix test

--- test_version.cpp
+++ test_version.cpp
@@ -1,4 +1,4 @@
-#define BOOST_TEST_MODULe test_version
+#define BOOST_TEST_MODULE test_version
 
 #include <boost/test/unit_test.hpp>
 
@@ -6,7 +6,11 @@
 
 BOOST_AUTO_TEST_SUITE(test_version)
 
-BOOST_AUTO_TEST_CASE(test_valid_version)
+BOOST_AUTO_TEST_CASE(test_version_valid)
 {
 	BOOST_CHECK(version() > 0);
 }
+
+BOOST_AUTO_TEST_SUITE_END();
+
+
